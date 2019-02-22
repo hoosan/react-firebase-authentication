b1d07e3403672203e4240b1f05959f79d6ce4575
hoosan <hoosanalis@gmail.com>
2019-02-23 02:41:13 +0900

    until page 35

--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link, withRouter } from 'react-router-dom';
+import { compose } from 'recompose';
 
 import { withFirebase } from '../Firebase';
 import * as ROUTES from '../../constants/routes';
@@ -108,7 +109,11 @@ const SignUpLink = () => (
   </p>
 );
 
-const SignUpForm = withRouter(withFirebase(SignUpFormBase));
+// const SignUpForm = withRouter(withFirebase(SignUpFormBase));
+const SignUpForm = compose(
+  withRouter,
+  withFirebase,
+)(SignUpFormBase);
 
 export default SignUpPage;
 
