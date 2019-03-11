43d26cac3a920b619d60613843c95588e5d82f81
hoosan <hoosanalis@gmail.com>
2019-03-12 07:10:47 +0900

    until page 102

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -17,6 +17,8 @@ class Firebase {
 
     this.auth = app.auth();
     this.db = app.database();
+
+    this.googleProvider = new app.auth.GoogleAuthProvider();
   }
 
   // *** Auth API ***
@@ -26,12 +28,16 @@ class Firebase {
   doSignInWithEmailAndPassword = (email, password) =>
     this.auth.signInWithEmailAndPassword(email, password);
 
+  doSignInWithGoogle = () =>
+    this.auth.signInWithPopup(this.googleProvider);
+
   doSignOut = () => this.auth.signOut();
 
   doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
 
   doPasswordUpdate = password =>
     this.auth.currentUser.updatePassword(password);
+
 
   // *** Merge Auth and DB User API *** //
 
