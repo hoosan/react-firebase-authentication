368e6f1151f1b113fd1505c0546973b6b93b8069
hoosan <hoosanalis@gmail.com>
2019-03-13 00:02:05 +0900

    until page 112

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -20,6 +20,7 @@ class Firebase {
 
     this.googleProvider = new app.auth.GoogleAuthProvider();
     this.facebookProvider = new app.auth.FacebookAuthProvider();
+    this.twitterProvider = new app.auth.TwitterAuthProvider();
   }
 
   // *** Auth API ***
@@ -34,6 +35,9 @@ class Firebase {
 
   doSignInWithFacebook = () =>
     this.auth.signInWithPopup(this.facebookProvider);
+
+  doSignInWithTwitter = () =>
+    this.auth.signInWithPopup(this.twitterProvider);
 
   doSignOut = () => this.auth.signOut();
 
