54fd2c92982139348a3c9e5bcecf8d0c4fc4e56d
hoosan <hoosanalis@gmail.com>
2019-03-23 22:11:18 +0900

    until page 145

--- src/components/Admin/index.js
+++ src/components/Admin/index.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
 import { compose } from 'recompose';
 
 import { withFirebase } from '../Firebase';
 import { withAuthorization, withEmailVerification } from '../Session';
 import * as ROLES from '../../constants/roles';
+import * as ROUTES from '../../constants/routes';
 
-class AdminPage extends Component {
+const AdminPage = () => (
+  <div>
+    <h1>Admin</h1>
+    <p>The Admin Page is accessible by every signed in admin user.</p>
+
+    <Switch>
+      <Route exact path={ROUTES.ADMIN_DETAILS} component={UserItem} />
+      <Route exact path={ROUTES.ADMIN} component={UserList} />
+    </Switch>
+  </div>
+);
+
+class UserListBase extends Component {
   constructor(props) {
     super(props);
 
@@ -43,38 +57,91 @@ class AdminPage extends Component {
 
     return (
       <div>
-        <h1>Admin</h1>
-        <p>
-          The Admin Page is accessible by every signed in admin user.
-        </p>
-
+        <h2>Users</h2>
         {loading && <div>Loading ...</div>}
-        <UserList users={users} />
+        <ul>
+          {users.map(user => (
+            <li key={user.uid}>
+              <span>
+                <strong>ID:</strong> {user.email}
+              </span>
+              <span>
+                <strong>E-mail:</strong> {user.email}
+              </span>
+              <span>
+                <strong>Username:</strong> {user.username}
+              </span>
+              <span>
+                <Link to={`${ROUTES.ADMIN}/${user.uid}`}>
+                  Details
+                </Link>
+              </span>
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
 }
 
-const UserList = ({ users }) => (
-  <ul>
-    {users.map(user => (
-      <li key={user.uid}>
-        <span>
-          <strong>ID:</strong> {user.email}
-        </span>
-        <span>
-          <strong>E-mail:</strong> {user.email}
-        </span>
-        <span>
-          <strong>Username:</strong> {user.username}
-        </span>
-      </li>
-    ))}
-  </ul>
-);
-
 const condition = authUser =>
   authUser && authUser.roles.includes(ROLES.ADMIN);
+
+class UserItemBase extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: false,
+      user: null,
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ loading: true });
+
+    this.props.firebase
+      .user(this.props.match.params.id)
+      .on('value', snapshot => {
+        this.setState({
+          user: snapshot.val(),
+          loading: false,
+        });
+      });
+  }
+
+  componentWillUnmount() {
+    this.props.firebase.user(this.props.match.params.id).off();
+  }
+
+  render() {
+    const { user, loading } = this.state;
+
+    return (
+      <div>
+        <h2>User ({this.props.match.params.id})</h2>
+        {loading && <div>Loading ...</div>}
+
+        {user && (
+          <div>
+            <span>
+              <strong>ID:</strong> {user.uid}
+            </span>
+            <span>
+              <strong>E-Mail:</strong> {user.email}
+            </span>
+            <span>
+              <strong>Username:</strong> {user.username}
+            </span>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+const UserList = withFirebase(UserListBase);
+const UserItem = withFirebase(UserItemBase);
 
 export default compose(
   withEmailVerification,
